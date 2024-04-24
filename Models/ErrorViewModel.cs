8b491f9d706bac2155ddd5942ded6a5dd5085d72
“SASHALIKgit config --global user.email “alexsun2006.ukr@gmail.com <alexsun2006.ukr@gmail.com>
2024-04-24 19:30:15 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CBS_ASP.NET_Core_Course_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
